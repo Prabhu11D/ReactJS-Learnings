7d232821a0e6f9ec114f5c2e1ab83a109e78d4a1
Prabhu K <52393935+programmerprabhu@users.noreply.github.com>
2021-01-14 11:50:31 +0530

    3.Class Component

--- Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
+++ Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Greet from './components/Greet';
 // import { Greet } from './components/Greet';
 
+import Welcome from './components/Welcome'
+
 function App() {
   return (
     <div className="App">
         <Greet />
+        <Welcome />
     </div>
   );
 }
