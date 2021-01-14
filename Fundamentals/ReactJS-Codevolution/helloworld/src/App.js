6b402e91c5a87ec0ccdab094e3b55928835b5f6e
Prabhu K <52393935+programmerprabhu@users.noreply.github.com>
2021-01-14 13:09:41 +0530

    5.Props

--- Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
+++ Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
@@ -9,9 +9,17 @@ import Hello from "./components/Hello";
 function App() {
     return (
         <div className="App">
-            <Greet />
-            <Welcome />
-            <Hello />
+            <Greet name="Prabhu" heroName = "Iron Man">
+                <p>Charge Me</p>
+            </Greet>
+            <Greet name="John" heroName="Thor">
+                <button>Rotate Me</button>
+            </Greet>
+            <Greet name="Jai Surya" heroName="Captain America">
+                <button>S.H.I.E.L.D</button>
+            </Greet>
+
+            <Welcome name="Jai Surya" heroName="Captain America"/>
         </div>
     );
 }
--- Fundamentals/ReactJS-Codevolution/helloworld/src/components/Greet.js
+++ Fundamentals/ReactJS-Codevolution/helloworld/src/components/Greet.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-// function Greet(){
-//     return <h1>Hello Prabhu</h1>
-// }
-
-const Greet = () => <h1>Hello Prabhu</h1>
-// export const Greet = () => <h1>Hello Prabhu</h1>
+const Greet = props => {
+    return (
+        <div>
+            <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+            {props.children}
+        </div>
+    )
+}
 
 export default Greet
