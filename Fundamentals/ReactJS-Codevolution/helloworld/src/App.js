a54d5112ba49b545c06dd312d1f54196af2d68a1
Prabhu K <52393935+programmerprabhu@users.noreply.github.com>
2021-01-14 17:30:09 +0530

    11.Methods as Props

--- Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
+++ Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
@@ -1,22 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Greet from "./components/Greet";
-// import { Greet } from './components/Greet';
 
-import Welcome from "./components/Welcome";
-import Hello from "./components/Hello";
-import Message from "./components/Message";
-import Counter from "./components/Counter";
-
-import FunctionClick from "./components/EventHandler/functionClick";
-import ClassClick from "./components/EventHandler/classClick";
-
-import EventBind from "./components/EventBind";
+import ParentComponent from "./components/Methods_as_Props/ParentComponent";
 
 function App() {
     return (
         <div className="App">
-            <EventBind />
+            <ParentComponent />
         </div>
     );
 }
