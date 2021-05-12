3503afb4a07ee3b9d99240114741d2684df440ba
Prabhu K <52393935+programmerprabhu@users.noreply.github.com>
2021-05-12 19:53:24 +0530

    Advice App by JavaSscript Mastery

---
+++ Projects/quotes/advice-app/src/App.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        this.setState({
+          advice: response.data.slip.advice,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
