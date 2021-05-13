aa1e24066e5cfe90a5cf3ade2807a96ddcc0d083
Prabhu K <52393935+programmerprabhu@users.noreply.github.com>
2021-05-13 17:26:41 +0530

    31. Portal

--- Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
+++ Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
-import ForwardRefComp from "./components/Refs/ForwardRefComp";
-import RefsDemo from "./components/Refs/RefsDemo";
-import Refs_with_Class_Component from "./components/Refs/Refs_with_Class_Component";
+import PortalDemo from "./components/PortalDemo";
 
 function App() {
-  // Refs only used in Class Component
-  // return <RefsDemo />;
-  // return <Refs_with_Class_Component />;
-  return <ForwardRefComp />;
+  return <PortalDemo />;
 }
 
 export default App;
