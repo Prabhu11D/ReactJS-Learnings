9c1fd72f180a96e20f83a8b40e8fcbf36cfc5c8c
Prabhu K <52393935+programmerprabhu@users.noreply.github.com>
2021-05-13 17:51:23 +0530

    32. Error Boundary

--- Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
+++ Fundamentals/ReactJS-Codevolution/helloworld/src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
-import PortalDemo from "./components/PortalDemo";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import Hero from "./components/ErrorBoundary/Hero";
 
 function App() {
-  return <PortalDemo />;
+  return (
+    <div>
+      <ErrorBoundary>
+        <Hero heroName="Superman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Batman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker" />
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
