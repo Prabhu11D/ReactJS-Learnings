ce1ce07371faf61ae26046959029e7dc2d9a96c2
Prabhu <programmerprabhu@gmail.com>
2021-06-15 12:36:50 +0530

    UI Components: DropDown -> Facebook Drop Down Menu by Fireship

--- Projects/UI-Components/drop-down/src/index.js
+++ Projects/UI-Components/drop-down/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchableDropDown from './searchable-drop-down';
+// import SearchableDropDown from './searchable-drop-down';
+import FaceBookDropDownMenu from './facebook-menu';
 
 function App() {
   return (
     <>
-      <SearchableDropDown />
+      {/* <SearchableDropDown /> */}
+      <FaceBookDropDownMenu />
     </>
   );
 }
