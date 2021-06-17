73c20ac6b0fe69b5bb6999bb07c8c6b2367d3f58
Prabhu <programmerprabhu@gmail.com>
2021-06-17 22:26:12 +0530

    UI-Components -> Range Slider -> Double Range Slider

---
+++ Projects/UI-Components/range-slider/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DoubleRangeSlider from './DoubleRangeSlider';
+
+function App() {
+  return (
+    <div className="App">
+      <DoubleRangeSlider />
+    </div>
+  );
+}
+
+export default App;
